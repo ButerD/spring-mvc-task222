9fb3d34e05561af3f94f99c943168d67577ba919
ButerD <buter81@gmail.com>
2021-09-12 23:01:57 +0300

    add init commit & test release

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,26 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class CarController {
+    CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String printCars(Model model, @RequestParam(value = "count", defaultValue = "5") int count) {
+        System.out.println(count);
+        model.addAttribute("carList", carService.getCars(count));
+        return "cars";
+    }
+}
